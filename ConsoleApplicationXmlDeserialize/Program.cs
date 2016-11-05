a801bc3878929de5f49832e8b68a63731f8f038f
weboni83 <weboni83@gmail.com>
2016-11-05 17:58:24 +0900

    MenuList 만들기 기능 추가
    메뉴 목록만 XmlList로 내보내기 기능 추가

--- ConsoleApplicationXmlDeserialize/Program.cs
+++ ConsoleApplicationXmlDeserialize/Program.cs
@@ -15,8 +15,10 @@ namespace ConsoleApplicationXmlDeserialize
     {
         static void Main(string[] args)
         {
+            string solutionPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string path = string.Format("{0}{1}", solutionPath, @"\Xml\Menu.xml");
             //string path = @"D:\작업중 자료\20161024 다국어지원\ZF1704.srf";
-            string path = @"D:\작업중 자료\20161024 다국어지원\Menu.xml";
+            //string path = @"D:\작업중 자료\20161024 다국어지원\Menu.xml";
             StringBuilder sbuilder = new StringBuilder();
             //DeSerialize
             string xml = File.ReadAllText(path);
@@ -40,24 +42,57 @@ namespace ConsoleApplicationXmlDeserialize
                 application = (MenuCollection.Application)serializer.Deserialize(reader);
             }
 
-            //Set 해보자
-            Dictionary<string, string> captionList = new Dictionary<string,string>();
-            captionList.Add("ZF0000", "큐피엘~");
-            //captionList.Add("ZF00001", "STANDARD INFO");
-            //captionList.Add("ZF0200", "Item INFO");
-            //captionList.Add("ZF0110Z", "Type Registration");
+            ////Set 해보자
+            //Dictionary<string, string> captionList = new Dictionary<string,string>();
+            //captionList.Add("ZF0000", "큐피엘~");
+            ////captionList.Add("ZF00001", "STANDARD INFO");
+            ////captionList.Add("ZF0200", "Item INFO");
+            ////captionList.Add("ZF0110Z", "Type Registration");
 
-            captionList.ToList().ForEach(x => application.Menus.Where(w => w.Menu.UniqueID.Equals(x.Key)).FirstOrDefault().Menu.String = x.Value);
+            //captionList.ToList().ForEach(x => application.Menus.Where(w => w.Menu.UniqueID.Equals(x.Key)).FirstOrDefault().Menu.String = x.Value);
 
+            //메뉴 리스트 (번역 기준 항목으로 사용하려고 함)
+            List<Menu> menus = new List<Menu>();
 
             ////2.Filterling --Get
-            //MenuCollection.ApplicationActionMenuMenus
-            var applicationAction = application.Menus.Where(x => x.type.Equals("add")).ToList<MenuCollection.ApplicationAction>();
+            application.Menus.ToList<MenuCollection.ApplicationAction>().ForEach(x => menus.AddRange(MenuParser(x)));
 
-            applicationAction.ForEach(x => Recursive(x));
+            menus.ForEach(x => Console.WriteLine(x.UniqueID));
 
+
+
+            //XML 만들기(Menu List)
+            XDocument xmldoc = new XDocument();
+            XElement element = new XElement("Menus");
+            menus.ForEach(x => element.Add(
+                x.Image != null ?
+                new XElement("Menu",
+                new XElement("Checked", x.Checked),
+                new XElement("Enabled", x.Enabled),
+                new XElement("FatherUID", x.FatherUID),
+                new XElement("Position", x.Position),
+                new XElement("String", x.String),
+                new XElement("Type", x.Type),
+                new XElement("UniqueID", x.UniqueID),
+                new XElement("Image", x.Image))
+                : new XElement("Menu",
+                new XElement("Checked", x.Checked),
+                new XElement("Enabled", x.Enabled),
+                new XElement("FatherUID", x.FatherUID),
+                new XElement("Position", x.Position),
+                new XElement("String", x.String),
+                new XElement("Type", x.Type),
+                new XElement("UniqueID", x.UniqueID))
+                ));
+
+            xmldoc.Add(element);
+            xmldoc.Save(string.Format("{0}{1}", solutionPath, @"\OutXml\MenuList.xml"));
+
+
+
+            //기존 XML 스트링만 변경해서 새로 만들기
             // wrap XmlTextWriter into a using block because it supports IDisposable
-            using (XmlTextWriter tw = new XmlTextWriter(@"D:\작업중 자료\20161024 다국어지원\NewMenu.xml", Encoding.UTF8))
+            using (XmlTextWriter tw = new XmlTextWriter(string.Format("{0}{1}", solutionPath, @"\OutXml\Menu.xml"), Encoding.UTF8))
             {
                 //줄바꿈 적용
                 tw.Formatting = Formatting.Indented;
@@ -124,22 +159,26 @@ namespace ConsoleApplicationXmlDeserialize
 
 
         }
-
-        private static void Recursive(MenuCollection.ApplicationAction appAction)
+        //Parser 역할
+        private static List<Menu> MenuParser(MenuCollection.ApplicationAction appAction)
         {
+            //return menus
+            List<Menu> menus = new List<Menu>();
+
             MenuCollection.ApplicationActionMenu appMenu = appAction.Menu;
 
             Console.WriteLine(string.Format("{0}, {1}", appMenu.UniqueID, appMenu.String));
+            menus.Add(new Menu(appMenu.Checked, appMenu.Enabled, appMenu.FatherUID, appMenu.Position, appMenu.String, appMenu.Type, appMenu.UniqueID, appMenu.Image));
 
             if (appMenu.Menus.Count() > 0)
             {
-                foreach (MenuCollection.ApplicationActionMenuMenus menus in appMenu.Menus)
+                foreach (MenuCollection.ApplicationActionMenuMenus app_menus in appMenu.Menus)
                 {
-                    
                     //MenuCollection.ApplicationActionMenu menus_menu = menus.Action.Menu;
-                    foreach (MenuCollection.ApplicationActionMenuMenusActionMenu menus_menu in menus.Action.Menu)
+                    foreach (MenuCollection.ApplicationActionMenuMenusActionMenu menus_menu in app_menus.Action.Menu)
                     {
                         Console.WriteLine(string.Format("-{0}, {1}", menus_menu.UniqueID, menus_menu.String));
+                        menus.Add(new Menu(menus_menu.Checked, menus_menu.Enabled, menus_menu.FatherUID, menus_menu.Position, menus_menu.String, menus_menu.Type, menus_menu.UniqueID));
 
                         if (menus_menu.Menus == null)
                             continue;
@@ -152,6 +191,7 @@ namespace ConsoleApplicationXmlDeserialize
                                 foreach (MenuCollection.ApplicationActionMenuMenusActionMenuMenusActionMenu men_menu in men.Action.Menu)
                                 {
                                     Console.WriteLine(string.Format("--{0}, {1}", men_menu.UniqueID, men_menu.String));
+                                    menus.Add(new Menu(men_menu.Checked, men_menu.Enabled, men_menu.FatherUID, men_menu.Position, men_menu.String, men_menu.Type, men_menu.UniqueID));
 
                                     if (men_menu.Menus == null)
                                         continue;
@@ -163,6 +203,7 @@ namespace ConsoleApplicationXmlDeserialize
                                     foreach (MenuCollection.ApplicationActionMenuMenusActionMenuMenusActionMenuMenusActionMenu me_menu in men_menu.Menus.Action.Menu)
                                     {
                                         Console.WriteLine(string.Format("---{0}, {1}", me_menu.UniqueID, me_menu.String));
+                                        menus.Add(new Menu(me_menu.Checked, me_menu.Enabled, me_menu.FatherUID, me_menu.Position, me_menu.String, me_menu.Type, me_menu.UniqueID));
 
                                     }
                                 }
@@ -171,6 +212,8 @@ namespace ConsoleApplicationXmlDeserialize
                     }
                 }
             }
+
+            return menus;
         }
     }
 }
